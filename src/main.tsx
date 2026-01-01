3942e52e7868e381506c2694d644e1d108afb72f
RhyanO2 <rhyanoliveirads@gmail.com>
2026-01-01 02:21:02 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <SpeedInsights />
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
