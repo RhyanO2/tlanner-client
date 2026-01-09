fb4f1b9d29ebbec3b941de18bc7e1c0cf9a7625a
RhyanO2 <rhyanoliveirads@gmail.com>
2026-01-09 17:34:31 -0300

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
 import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SpeedInsights />
+    <Analytics />
     <BrowserRouter>
       <App />
     </BrowserRouter>
